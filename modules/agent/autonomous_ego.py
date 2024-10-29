afb7e8d31698baebe98d43e9d0ded0b2f3ae26ef
Luca Garello <luca.garello@iit.it>
2024-10-29 18:01:57 +0100

    First test of planner

--- modules/agent/autonomous_ego.py
+++ modules/agent/autonomous_ego.py
@@ -16,25 +16,40 @@ LANGCHAIN_API_KEY=os.getenv("LANGCHAIN_API_KEY")
 AZURE_OPENAI_API_KEY=os.getenv("AZURE_OPENAI_API_KEY")
 AZURE_OPENAI_ENDPOINT= os.getenv("AZURE_OPENAI_ENDPOINT")
 
+objects = ["cleanser","sponge","bowl", "plate", "mug", "fork", "knife", "spoon","chocolate_jello", "strawberry_jello", "coffee_grounds", "mustard", "spam", "sugar", "tomato_soup", "tuna","bowl", "plate", "mug", "fork", "knife", "spoon","cleanser",
+            "sponge","apple", "banana", "lemon", "orange", "peach", "pear", "plum", "strawberry", "cheezit", "cornflakes", "pringles","baseball", "dice", "rubiks_cube", "soccer_ball", "tennis_ball"]
+
 # Define the tools for the agent to use
 @tool
-def go_to_location(query: str):
-    """call to navigate to a location."""
-    click.secho(f"ACTION: going to <{query}>", fg='green', bold=True, italic=True)
-    # with click.progressbar([1, 2, 3]) as bar:
-    #     for x in bar:
-    #         #print(f"sleep({x})...")
-    #         time.sleep(x)
-    return True
+def go_to_location(location: str):
+    """
+    Call to navigate to a location. It can be INRIA, KIT or DLR. Call this before pick() or place() tools. call this tool only once.
+    
+    Args:
+        location (str): INRIA, KIT or DLR
+    """
+
+    click.secho(f"ACTION: going to <{location}>", fg='green', bold=True, italic=True)
+    if query.lower() in ["inria", "kit", "dlr"]:
+        return True
+    else:
+        click.secho(f"ERROR: invalid location <{query}>, choose between INRIA, KIT or DLR", fg='red', bold=True, italic=True)
+        return False
 
 @tool
-def go_to_sublocation(query: str):
-    """call to select from which area of the location to pick an object. It can be Dishwasher, Table or Cabinet. Call this after go_to_location."""
-    click.secho(f"ACTION: selecting area <{query}>", fg='green', bold=True, italic=True)
+def go_to_sublocation(sublocation: str):
+    """
+    Call to select from which area of the location to pick an object. call this tool only once.
+    Args:
+        query (str): Dishwasher, Table or Cabinet
+    """
+
+
+    click.secho(f"ACTION: selecting area <{sublocation}>", fg='green', bold=True, italic=True)
     if query.lower() in ["dishwasher", "table", "cabinet"]:
         return True
     else:
-        click.secho(f"ERROR: invalid sublocation <{query}>, choose between Dishwasher,Table or Cabinet", fg='red', bold=True, italic=True)
+        click.secho(f"ERROR: invalid sublocation <{sublocation}>, choose between Dishwasher,Table or Cabinet", fg='red', bold=True, italic=True)
         return False
 
 @tool
@@ -61,7 +76,39 @@ def understand_scene(query: str):
     click.secho(f"ACTION: understanding the prompt based on: {query}]", fg='green', bold=True, italic=True)
     return True
 
-tools = [go_to_location, go_to_sublocation, pick, place, give, understand_scene]
+@tool
+def execute_plan(object: str, origin_location: str, origin_sublocation: str, target_location: str, target_sublocation: str):
+    """call this tool when the plan is ready to be executed
+    Args:
+        object (str): object to pick referred as O
+        origin_location (str): location to go (INRIA, KIT or DLR) referred K_origin
+        origin_sublocation (str): sublocation to pick object from (Dishwasher, Table or Cabinet) referred as L_origin 
+        target_location (str): location to place object (INRIA, KIT or DLR) referred as K_target
+        target_sublocation (str): sublocation to place object (Dishwasher, Table or Cabinet, person) referred as L_target
+    """
+    print("\n")
+    click.secho("[ACTION:] THE PLAN IS SET", fg='green', bold=True)
+    click.secho("Moving", nl=False)
+    click.secho(f" {object} ", fg='yellow', bold=True, nl=False)
+    click.secho("from", nl=False)
+    click.secho(f" {origin_location} ", fg='blue', bold=True, nl=False)
+    click.secho(f" {origin_sublocation} ", fg='red', bold=True, nl=False)
+    click.secho(f"to", nl=False)
+    click.secho(f" {target_location} ", fg='blue', bold=True, nl=False)
+    click.secho(f" {target_sublocation} ", fg='red', bold=True)
+    print("\n")
+
+    return True
+
+#Pick the jello from the DLR cabinet and place it on the INRIA cabinet
+
+# @tool
+# def execute_plan():
+#     """call this tool when the plan is ready to be executed"""
+#     click.secho(f"ACTION: ready to go", fg='green', bold=True, italic=True)
+#     return True 
+
+tools = [execute_plan]
 
 action_node = ToolNode(tools)
 
@@ -141,9 +188,10 @@ def chat_with_robot():
             if user_input.lower() in ["exit", "quit"]:
                 print("Exiting chat.")
                 break
-
+            
+            #Pick Object Apple at Location Table in Kitchen INRIA
             current_state = robot.invoke(
-                {"messages": [SystemMessage("esegui task scomponendoli in azioni che siano di questo tipo GO_TO_LOCATION() GO_TO_SUBLOCTION() , PICK(), PLACE()/GIVE(). Ad esempio GO_TO_LOCATION(bagno) GO_TO_SUBLOCTION(vasca) , PICK(mela), PLACE()") , HumanMessage(content=user_input)]},
+                {"messages": [SystemMessage(f"Given a prompt in the format: Pick Object 'O' at Location 'L_origin' in Kitchen 'K_origin' and place it on 'K_target' 'L_target'. Where O={{{', '.join(objects)}}}, L_target={{Dishwasher, Table, Cabinet}}, L_origin={{Dishwasher, Table, Cabinet, person}} K_target={{DLR, KIT, INRIA}} and K_origin={{DLR, KIT, INRIA}}. Extract O, L_origin, L_target, K_origin and K target. if you are not sure about one component ask for confirmation"), HumanMessage(content=user_input)]},
                 config={"configurable": {"thread_id": 42}},
             )
 
